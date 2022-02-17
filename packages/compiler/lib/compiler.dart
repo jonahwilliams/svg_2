85c2da77e615a92274699cc44b8b02880c3ae457
Dan Field <dnfield@google.com>
2022-02-17 09:28:15 -0800

    It compiles

--- packages/compiler/lib/compiler.dart
+++ packages/compiler/lib/compiler.dart
@@ -2,4 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-void main() {}
+import 'dart:io';
+
+import 'package:compiler/src/parser/parser_state.dart';
+import 'package:compiler/src/parser/picture_stream.dart';
+import 'package:xml/xml_events.dart';
+
+void main() async {
+  final String xml = File('Ghostscript_Tiger.svg').readAsStringSync();
+  final SvgParserState state = SvgParserState(
+    parseEvents(xml),
+    const SvgTheme(),
+    'testing',
+    true,
+  );
+  final root = await state.parse();
+  root.write();
+}
